e07819bad6522df2f020099fc8800c068e01e9da
chenchiaho <chenchiaho@gmail.com>
2023-03-30 00:36:08 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import Navbar from "./scenes/navbar"
+import { useEffect, useState } from "react";
+
+function App() {
+
+  const [selectedPage, setSelectedPage] = useState<string>('home')
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ src/scenes/navbar/Link.tsx
@@ -0,0 +1,26 @@
+import AnchorLink from "react-anchor-link-smooth-scroll"
+
+type Props = {
+  page: string
+  selectedPage: SelectedPage
+  setSelectedPage: (value: SelectedPage) => void
+}
+
+const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
+
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "")
+
+  return (
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""} trasition duration-500 hover:text-primary-300
+      `}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+
+  )
+}
+
+export default Link
