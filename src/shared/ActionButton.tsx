f37a658d8d028b3fbc972b71a273aa031b49c27f
chenchiaho <chenchiaho@gmail.com>
2023-04-02 02:06:51 +0800

    implemented homepage

--- src/shared/ActionButton.tsx
+++ src/shared/ActionButton.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { SelectedPage } from './types'
 
-type Props = {}
+type Props = {
+  children: React.ReactNode
+  setSelectedPage: (value: SelectedPage) => void
+}
 
-const ActionButton = (props: Props) => {
+const ActionButton = ({ children, setSelectedPage }: Props) => {
   return (
-    <div>ActionButton</div>
+    <AnchorLink
+      className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white"
+      onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+      href={`#${SelectedPage.ContactUs}`}
+    >
+      {children}
+    </AnchorLink>
   )
 }
 
